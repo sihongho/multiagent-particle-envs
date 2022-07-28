1f53a1044d0a37eebb11980b053aad3bc8a3779d
Sihong He <Sihong.he@uconn.edu>
2022-07-27 23:27:59 -0400

    Update simple_spread_multi_agent.py

--- multiagent/scenarios/simple_spread_multi_agent.py
+++ multiagent/scenarios/simple_spread_multi_agent.py
@@ -4,7 +4,7 @@ from multiagent.scenario import BaseScenario
 
 
 class Scenario(BaseScenario):
-    def make_world(self, num_agents = 15):
+    def make_world(self, num_agents = 15, num_adversaries = 0, num_landmarks = 0):
         world = World()
         # set any world properties first
         world.dim_c = 2
